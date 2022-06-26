c9b5b87badb1a5fa3c361b74f6a6a07448df7c87
WenceslasSANCHEZ <wenceslas.sanchez@compass-ft.com>
2022-06-26 14:40:22 +0200

    ➕ add local crates

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,5 @@ mod front_end;
 mod utils;
 mod back_end;
 
-pub use front_end::{input_db, parse_input, execute_statement};
+pub use front_end::{input_db, parse_input, execute_statement, Table, InputParsed};
 pub use utils::{logger_builder};
-pub use back_end::{Table};
