7327146a98761c624c4a089a238f77bdc5d6e2f4
WenceslasSANCHEZ <wenceslas.sanchez@compass-ft.com>
2022-06-26 14:39:13 +0200

    ⚡️ insert data to table

--- src/front_end.rs
+++ src/front_end.rs
@@ -1,5 +1,10 @@
 use std::io::{self, BufRead, Write};
+use std::str;
 use std::fmt;
+
+#[path="./back_end.rs"] mod back_end;
+pub use back_end::{Table, Row};
+
 
 pub enum InputAction {
     Create,
@@ -9,13 +14,13 @@ pub enum InputAction {
     Exit,
 }
 
-pub struct InputParsed<'a> {
+pub struct InputParsed {
     pub action: InputAction,
-    pub data: Box<Vec<&'a str>>,
+    pub data: Box<Vec<String>>,
 }
 
-impl InputParsed<'_> {
-    pub fn new(action: InputAction, data: Box<Vec<&str>>) -> InputParsed {
+impl InputParsed {
+    pub fn new(action: InputAction, data: Box<Vec<String>>) -> InputParsed {
         InputParsed {
             action,
             data
@@ -23,7 +28,6 @@ impl InputParsed<'_> {
     }
 }
 
-#[derive(Debug, Clone)]
 pub struct InputError;
 
 impl fmt::Display for InputError {
@@ -43,7 +47,11 @@ pub fn parse_input(input: &str) -> Result<InputParsed, InputError> {
     let split= input.split(' ').collect::<Vec<&str>>();
 
     let input_action= split[0];
-    let data= Box::new(split[1..].to_vec());
+    let data: Box<Vec<String>>= Box::new(split[1..]
+        .iter()
+        .map(|s| String::from(&**s))
+        .collect()
+    );
 
     match input_action {
         "create" => Ok(InputParsed::new(InputAction::Create, data)),
@@ -56,12 +64,40 @@ pub fn parse_input(input: &str) -> Result<InputParsed, InputError> {
 }
 
 
-pub fn execute_statement(input: InputParsed) {
+pub fn execute_statement(input: InputParsed, table: &mut Table) -> Result<i8, InputError> {
     match input.action {
-        InputAction::Create => println!("Create statement {:?}", input.data),
-        InputAction::Delete => println!("Delete statement {:?}", input.data),
-        InputAction::Insert => println!("Insert statement {:?}", input.data),
-        InputAction::Select => println!("Select statement {:?}", input.data),
+        InputAction::Create =>
+            {
+                println!("Create statement table {:?}", input.data[0]);
+                Ok(1)
+            },
+        InputAction::Delete =>
+            {
+                println!("Delete statement {:?}", input.data);
+                Ok(1)
+            },
+        InputAction::Insert =>
+            {
+                let result_id = input.data[0].clone().parse::<i8>();
+                if result_id.is_err() {
+                    return Err(InputError)
+                };
+
+                let row= Row::new(
+                    result_id.unwrap(),
+                    input.data[1].clone(),
+                    input.data[2].clone(),
+                );
+                table.append(Box::new(row));
+
+                println!("Insert statement {:?}", input.data);
+                Ok(table.num_element)
+            },
+        InputAction::Select =>
+            {
+                println!("Select statement {:?}", input.data);
+                Ok(1)
+            },
         InputAction::Exit => {
             println!("Exit statement");
             std::process::exit(0);
